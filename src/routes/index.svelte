1063b9700df9d12d7d671bb7929d52b086e03cff
yongsheng-telebort <yongsheng.telebort@gmail.com>
2022-02-25 15:54:00 +0000

    start project

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import supabase from '$lib/db';
+
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
+<!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+</section>	
